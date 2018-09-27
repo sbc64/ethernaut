8c7bb58a1da6b1f7fca8e7e499c62e31edb5952f
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-27 15:18:52 -0700

    Fix account generation

--- src/provider.js
+++ src/provider.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
+const bip39 = require('bip39')
 const Web3 = require('web3')
 const hdkey = require('ethereumjs-wallet/hdkey')
-const w3 = new Web3('https://ropsten.infura.io/' + process.env.INFURA_API)
 
-const pk = hdkey.fromMasterSeed(
-    process.env.MNEMONIC
-  ).getWallet().getPrivateKey()
-
-const account = w3.eth.accounts.privateKeyToAccount(pk)
+const wallet = hdkey
+  .fromMasterSeed(bip39.mnemonicToSeed(process.env.MNEMONIC))
+  .derivePath("m/44'/60'/0'/0/0")
+  .getWallet()
 
 module.exports = {
-  account,
-  w3
+  account: w3.eth.accounts.decrypt(wallet.toV3(''), ''),
+  w3:new Web3('https://ropsten.infura.io/' + process.env.INFURA_API)
 }
