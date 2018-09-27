c6ca76ecdc904ba08d63caba062d15226621fd47
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-27 16:55:09 -0700

    Win coinflip

--- src/challenges/CoinFlip.js
+++ src/challenges/CoinFlip.js
@@ -8,30 +8,54 @@ const instance = new w3.eth.Contract(CoinFlip.abi, addr)
 var FACTOR = w3.utils.toBN('57896044618658097711785492504343953926634992332820282019728792003956564819968')
 
 
-var lastBlock = 0
-w3.eth.getBlock('latest').then(block => {
-  lastBlock = block
-})
-
 const broadcastTxn = async guess => {
 
-  console.log('Gueess', guess)
+  const txn = {
+    data: await w3.eth.abi.encodeFunctionCall(CoinFlip.abi[2], [guess]),
+    from: account.address,
+    gas: 51000,
+    gasPrice: await w3.utils.toWei('0.000001'),
+    nonce: await w3.eth.getTransactionCount(account.address),
+    to: addr,
+  }
+  const rawTxn = await w3.eth.accounts.signTransaction(txn, account.privateKey)
+
+  return w3.eth.sendSignedTransaction(rawTxn.rawTransaction)
+}
+
+const sleep = (ms) => {
+  return new Promise(resolve=>{
+    setTimeout(resolve, ms)
+  })
 }
 
 const main = async () => {
-  console.log('MAAAIN', 
-  const block = await w3.eth.getBlock('latest')
-  const blockValue = w3.utils.toBN(block.hash)
+  console.log('MAAAIN')
+  let lastBlock = await w3.eth.getBlock('latest')
+  while (true) {
+    const block = await w3.eth.getBlock('latest')
+    const blockValue = w3.utils.toBN(block.hash)
 
-  if (block === lastBlock) {
-    console.log('Same block')
-  } else {
-    broadcastTxn(blockValue.div(FACTOR).toNumber() === 1 ? true : false)
+    if (block.number === lastBlock) {
+      console.log('Same block')
+    } else {
+      console.log('Old block: ', lastBlock.number)
+      console.log('New block: ', block.number)
+      lastBlock = block.number
+      const receipt = await broadcastTxn(blockValue.div(FACTOR).toNumber() === 1)
+      console.log('Accepted block:', receipt.blockNumber)
+    }
+
+    const wins = await instance.methods.consecutiveWins().call()
+    console.log('Wins', wins)
+    if (wins === 10) {
+      console.log('DONE')
+      break
+    }
+
+    console.log()
+    await sleep(500)
   }
 }
 
-
-while (true) {
-  var timeout = 5000
-  setTimeout(main, timeout)
-}
+main()
--- src/provider.js
+++ src/provider.js
@@ -3,6 +3,8 @@ const bip39 = require('bip39')
 const Web3 = require('web3')
 const hdkey = require('ethereumjs-wallet/hdkey')
 
+const w3 = new Web3('https://ropsten.infura.io/' + process.env.INFURA_API)
+
 const wallet = hdkey
   .fromMasterSeed(bip39.mnemonicToSeed(process.env.MNEMONIC))
   .derivePath("m/44'/60'/0'/0/0")
@@ -10,5 +12,5 @@ const wallet = hdkey
 
 module.exports = {
   account: w3.eth.accounts.decrypt(wallet.toV3(''), ''),
-  w3:new Web3('https://ropsten.infura.io/' + process.env.INFURA_API)
+  w3
 }
